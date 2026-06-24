c1176ab3b676bf5817a1138c870cff383d01120d
Manjot Singh <msingh@blockstream.com>
2026-06-24 11:54:55 +0200

    removed hard waits

--- src/pages/filters.py
+++ src/pages/filters.py
@@ -4,13 +4,13 @@ class Filters:
         self.select_box = '[data-test="product-sort-container"]'
 
     async def sort_atoz(self):
-        await self.page.select_option(self.select_box, value='az', timeout=1000)
+        await self.page.select_option(self.select_box, value='az')
 
     async def sort_ztoa(self):
-        await self.page.select_option(self.select_box, value='za', timeout=1000)
+        await self.page.select_option(self.select_box, value='za')
 
     async def sort_lohi(self):
-        await self.page.select_option(self.select_box, value='lohi', timeout=1000)
+        await self.page.select_option(self.select_box, value='lohi')
 
     async def sort_hilo(self):
-        await self.page.select_option(self.select_box, value='hilo', timeout=1000)
+        await self.page.select_option(self.select_box, value='hilo')
